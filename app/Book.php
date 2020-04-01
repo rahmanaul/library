c47f8922795dc5efdbd496e837d8530ae8b6fdb0
Aulia Rahman <gumgumboy20@gmail.com>
2020-04-01 09:23:22 +0700

    Book Reservations

--- app/Book.php
+++ app/Book.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use mysql_xdevapi\Exception;
 
 class Book extends Model
 {
@@ -14,10 +15,40 @@ class Book extends Model
         return '/books/' . $this->id;
     }
 
+    public function checkout($user)
+    {
+        $this->reservations()->create([
+            'user_id' => $user->id,
+            'checked_out_at' => now(),
+        ]);
+    }
+
+    public function checkin($user)
+    {
+        $reservation = $this->reservations()->where('user_id', $user->id)
+            ->whereNotNull('checked_out_at')
+            ->whereNull('checked_in_at')
+            ->first();
+
+        if (is_null($reservation)){
+            throw new \Exception();
+        }
+
+        $reservation->update([
+            'checked_in_at' => now(),
+        ]);
+    }
+
     public function setAuthorIdAttribute($author)
     {
         $this->attributes['author_id'] = (Author::firstOrCreate([
             'name' => $author,
         ]))->id;
     }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
+
 }
